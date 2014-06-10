4b860ef48dc3a35ef382527bf934174b20e63216
inodb <ino.debruijn@scilifelab.se>
2014-06-10 13:40:49 +0200

    Remove debug toolbar from development settings

--- lims_project/lims_project/settings/development.py
+++ lims_project/lims_project/settings/development.py
@@ -52,25 +52,25 @@ CACHES = {
 
 ########## TOOLBAR CONFIGURATION
 # See: https://github.com/django-debug-toolbar/django-debug-toolbar#installation
-INSTALLED_APPS += (
-    'debug_toolbar',
-)
-
-# See: https://github.com/django-debug-toolbar/django-debug-toolbar#installation
-INTERNAL_IPS = ('127.0.0.1',)
-
-# See: https://github.com/django-debug-toolbar/django-debug-toolbar#installation
-MIDDLEWARE_CLASSES += (
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
-)
-
-DEBUG_TOOLBAR_PATCH_SETTINGS = False
-# See: https://github.com/django-debug-toolbar/django-debug-toolbar#installation
-DEBUG_TOOLBAR_CONFIG = {
-    'INTERCEPT_REDIRECTS': False,
-    'SHOW_TEMPLATE_CONTEXT': True,
-    'DEBUG_TOOLBAR_PATCH_SETTINGS': False,
-}
+#INSTALLED_APPS += (
+#    'debug_toolbar',
+#)
+#
+## See: https://github.com/django-debug-toolbar/django-debug-toolbar#installation
+#INTERNAL_IPS = ('127.0.0.1',)
+#
+## See: https://github.com/django-debug-toolbar/django-debug-toolbar#installation
+#MIDDLEWARE_CLASSES += (
+#    'debug_toolbar.middleware.DebugToolbarMiddleware',
+#)
+#
+#DEBUG_TOOLBAR_PATCH_SETTINGS = False
+## See: https://github.com/django-debug-toolbar/django-debug-toolbar#installation
+#DEBUG_TOOLBAR_CONFIG = {
+#    'INTERCEPT_REDIRECTS': False,
+#    'SHOW_TEMPLATE_CONTEXT': True,
+#    'DEBUG_TOOLBAR_PATCH_SETTINGS': False,
+#}
 ########## END TOOLBAR CONFIGURATION
 
 ########## STATIC CONFIGURATION
